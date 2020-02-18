1289e4d41991b332a9d4be88ea143d8f2f12f881
LizCelestial <celestelyse123@gmail.com>
2020-02-18 15:53:10 +0200

    added another message

--- src/main/java/Communication.java
+++ src/main/java/Communication.java
@@ -1,6 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Communication {
 
     public static void main(String[] args) {
         System.out.println("Hello There");
+
+        System.out.println("Here is my first maven project");
     }
 }
